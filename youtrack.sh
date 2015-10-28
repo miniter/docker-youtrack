e36fee0331c12a540cd03140d5ef7505670f8903
Wayne Brantley <wayne@waynebrantley.com>
2015-10-28 09:09:01 -0400

    remove ssl as use nginx in front

--- youtrack.sh
+++ youtrack.sh
@@ -5,7 +5,6 @@ if "x${YOUTRACK_BASE_URL}" != "x"; then
 		-XX:MaxMetaspaceSize=250m -Xmx1g \
 		-Duser.home=/var/lib/youtrack \
 		-Djava.security.egd=/dev/zrandom \
-		-Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit \
 		-Djetbrains.youtrack.disableBrowser=true -Djetbrains.youtrack.enableGuest=false \
 		-Djetbrains.mps.webr.log4jPath=/etc/youtrack/log4j.xml -Djava.awt.headless=true \
 		-Djetbrains.youtrack.baseUrl=${YOUTRACK_BASE_URL}
@@ -15,7 +14,6 @@ else
 		-XX:MaxMetaspaceSize=250m -Xmx1g \
 		-Duser.home=/var/lib/youtrack \
 		-Djava.security.egd=/dev/zrandom \
-		-Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit \
 		-Djetbrains.youtrack.disableBrowser=true -Djetbrains.youtrack.enableGuest=false \
 		-Djetbrains.mps.webr.log4jPath=/etc/youtrack/log4j.xml -Djava.awt.headless=true \
 		-jar /usr/local/youtrack/youtrack.jar 8080
